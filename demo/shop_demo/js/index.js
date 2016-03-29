2a1c5315418ddc4fcc0f72e68d880e9edc07cbf0
keyu <623212389@qq.com>
2016-03-29 21:12:51 +0800

    2016年3月上线版本

---
+++ demo/shop_demo/js/index.js
@@ -0,0 +1,144 @@
+﻿$(function(){
+	//搜索框
+	$("#search").keyup(function(e){
+		if(e.which==13)
+		{
+			alert("根本什么都没有你还搜搜搜。。。");
+		}
+		})
+	//换肤	
+	$("#color li").click(function(){
+		$(this).addClass("selected").siblings().removeClass("selected");
+		$("#cssfile").attr("href","css/skin_css/"+this.id+".css");
+		})
+	//导航栏
+	$("#nav li").mouseover(function(){
+		$(this).find("div").show();
+		}).mouseout(function(){
+			$(this).find("div").hide();
+			})
+	//热门
+	$(".hot").append("<img src='images/hot.gif' id='imghot'/>");
+	//大屏广告
+	var index=0;
+	var length=$("#title a").length;
+    var newhref = $("#ad a").eq(index).attr("href");
+	function a(x){
+		$("#ad a").attr("href",newhref).
+            find("img").eq(x).stop(true,true).fadeIn().siblings().fadeOut();
+		$("#title a").eq(x).addClass("selected").siblings().removeClass("selected");
+	}
+	$("#title a").mouseover(function(){
+		index=$(this).index();
+		a(index);
+		}).eq(0).mouseover();
+	$("#ad").mouseover(function(){
+		clearInterval(show);
+		}).mouseout(function(){
+			show=setInterval(function(){
+				a(index);
+				index++
+				if(index==length)index=0;
+				},3000)
+			}).mouseout();
+	//公告栏
+	var x=10;
+	var y=20;
+	$("#section_3 ul li a").mouseover(function(e){
+		var a="<div class='post'>"+$(this).text()+"</div>";
+		$(a).appendTo("body");
+		$(".post").css({
+			"left":(e.pageX+x)+"px",
+			"top":(e.pageY+y)+"px"
+			}).show("fast");
+		}).mouseout(function(){
+			$(".post").remove();
+			}).mousemove(function(e){
+				$(".post").css({
+				"left":(e.pageX+x)+"px",
+				"top":(e.pageY+y)+"px"
+			})
+	})
+	//图片滚动
+	$("#hd ul li").click(function(){
+		$(this).addClass("selected").siblings().removeClass("selected");
+		$("#list ul").stop(true,false).animate({left:-$("#list").width()*$(this).index()},"slow");
+		return false;
+		})
+	//放大镜效果
+	$("#list img").mouseover(function(){
+		$(this).closest("li").append("<div class='zoom'></div>");	
+		});
+		$(document).on("mouseout",".zoom",function(){
+			$(this).remove();
+			})
+	//小图切换大图
+	$("#menu img").click(function(){
+		$(this).addClass("selected").parent().siblings().find("img").removeClass("selected");
+		var n=$(this).attr("src").indexOf(".jpg");
+		$("#img_small").attr("src",$(this).attr("src").substring(0,n)+"_small.jpg");
+		})
+	//颜色切换
+	$("#detail_color img").click(function(){
+		$(this).addClass("selected").parent().siblings().find("img").removeClass("selected");
+		$("#col").text($(this).attr("alt"));
+		var n=$(this).attr("src").lastIndexOf(".");
+		var m=$(this).attr("src").lastIndexOf("/");
+		var str=$(this).attr("src").substring(m+1,n);
+		$("#menu li img").hide();
+		$("#menu").find("."+str).show();
+		$("#menu").find("."+str).eq(0).click();
+		}).eq(0).click();
+	//内容切换
+	$("#content li").click(function(){
+		$(this).addClass("selected").siblings().removeClass("selected");
+		$(".content_bd").eq($(this).index()).show().siblings(".content_bd").hide();
+		}).eq(0).click();
+	//尺寸选择
+	$("#cicun li").click(function(){
+		$(this).addClass("selected").siblings().removeClass("selected");
+		$("#ci").text($(this).text());
+		})
+	//总价计算
+	var price=$("#ji").text();
+	$("select").change(function(){
+		$("#ji").text($(this).val()*price);
+		})
+	//星级评分
+	$("#detail_pinfeng li").click(function(){
+		alert("您给此商品打的是："+$(this).attr("title"));
+		$("#detail_pinfeng").removeClass().addClass(this.id);
+		})
+	//点击查看大图
+	$("#look").click(function(){
+		var url=$("#menu li").find(".selected").attr("src").substring(0,$("#menu li").find(".selected").attr("src").lastIndexOf("."))+"_big.jpg";
+		var a="<div id='ooo_o'></div><div id='ooo'><div id='ooo_1'><img id='ooo_img' src='"+url+"' alt='' /><div id='ooo_text'></div></div></div>";
+		$("body").append(a);
+		var width=$("body").width();
+		var height=$("body").height();
+		$("#ooo").css({
+			"width":width,
+			"height":height
+			}).fadeIn();
+		$("#ooo_o").css({
+			"width":width,
+			"height":height
+			}).animate({opacity:"0.4"},400);
+			
+		})
+	$(document).on("click","#ooo",function(){
+		$(this).animate({opacity:"0"},1000,function(){
+			$(this).remove();
+			})
+		$("#ooo_o").animate({opacity:"0"},1000,function(){
+			$(this).remove();
+			})
+		})
+	//购物车合计
+	$("#shopping img").click(function(){
+		var a="谢谢惠顾！\n您选择的颜色是："+$("#col").text()+"; \n您选择的尺寸是："+$("#ci").text()+"码; \n您选择的数量是："+$("select").val()+"个; \n总价是："+$("#ji").text()+"元。";
+		if($("#ci").text()=="未选择"){alert("您的尺寸，哦不。。是您衣服的尺寸还没选择。。")}
+			else if(typeof($("#detail_pinfeng").attr("class"))=="undefined"){alert("您还没有给我们评分呢。。")}
+			else{alert(a)}
+		})
+})
---
+++ js/index.js
@@ -0,0 +1,284 @@
+﻿$(function(){
+	$.ajax({
+		type: "GET",
+		url: "ajax/message.json",
+		dataType: "json",
+		success: function(data){
+			for(var i in data.about_info){
+				$("#about_info").append("<p>"+data.about_info[i]+"</p>");
+			}
+			$("#skill_info").append("<p>"+data.skill_info+"</p>");
+			$(".skill_int").append("<ul></ul>");
+			for(var i in data.seo){
+				$("#skill_int1 ul").append("<li>"+data.seo[i]+"</li>");
+			}
+			for(var i in data.html){
+				$("#skill_int2 ul").append("<li>"+data.html[i]+"</li>");
+			}
+			for(var i in data.css){
+				$("#skill_int3 ul").append("<li>"+data.css[i]+"</li>");
+			}
+			for(var i in data.javaScript){
+				$("#skill_int4 ul").append("<li>"+data.javaScript[i]+"</li>");
+			}
+			$("#exp_info").append("<p>"+data.exp_info+"</p>");
+			for(var i in data.contact_info){
+				$("#contact_info").append("<p>"+data.contact_info[i]+"</p>");	
+			}
+		}
+	});
+    $('#dowebok').fullpage({
+		scrollingSpeed: 400,
+		css3: true,
+		resize: true,
+		anchors: ["page1","page2","page3","page4","page5","page6"],
+		verticalCentered: false,
+		afterRender: function(){
+			$("#home").css({"display":"block"}).addClass("home_zoom");
+			$("aside").css({"top":($(".active").height()-$("aside").height())/2});
+			$("header").before("<div id='header' style='opacity:0'></div>");	
+			$("#home_head").css({"margin-top":"100px"});
+			$("header").animate({opacity:"1"},1000,function(){
+				$("#header").css({"opacity":"0.3"});
+				$("#home_info1").fadeIn(700,function(){
+					$(this).next().animate({width:"800px"},700,function(){
+						$("#home_info2").fadeIn(450,function(){
+							$(this).next().fadeIn(450,function(){
+								$(this).next().fadeIn(450,function(){
+									$(this).next().fadeIn(450,function(){
+										$("aside").fadeIn(300);
+									});
+								});
+							});
+						});
+					});
+				});
+			});	
+			$("aside a").eq(0).addClass("selected").siblings().removeClass("selected");
+		},
+		afterLoad: function(anchorLink,index){
+			if(index==1){
+				$("aside a").eq(0).addClass("selected").siblings().removeClass("selected");
+			}
+			if(index==2){
+				$("aside a").eq(1).addClass("selected").siblings().removeClass("selected");
+				$("#about_content h1").after("<div class='title_en'><h2>· About me ·</h2></div>");
+				$(".title_en").animate({width:"130px"},800,function(){
+					$(".title_en h2").slideDown(400);
+				});
+				$("#about_info").animate({width:"800px",marginTop:"0",marginBottom:"0"},700,'easeOutElastic',function(){
+					$("#about_info p").eq(0).animate({bottom:"0"},700,function(){
+						$("#about_info p").eq(1).animate({bottom:"0"},700,function(){
+							$("#about_info p").eq(2).animate({bottom:"0"},700,function(){
+								$("#about_info p").eq(3).animate({bottom:"0"},700);
+							});
+						});
+					});
+				});	
+			}
+			if(index==3){
+				$("aside a").eq(2).addClass("selected").siblings().removeClass("selected");
+				$("#skill_content h1").after("<div class='title_en'><h2>· Skill ·</h2></div>");
+				$(".title_en").animate({width:"130px"},800,function(){
+					$(".title_en h2").slideDown(400);
+				});	
+				$(".skill_list_content").addClass("skill_scale");
+			}
+			if(index==4){
+				$("aside a").eq(3).addClass("selected").siblings().removeClass("selected");
+				$("#exp_content h1").after("<div class='title_en'><h2>· Experience ·</h2></div>");
+				$(".title_en").animate({width:"130px"},800,function(){
+					$(".title_en h2").slideDown(400);
+				});	
+				var i=-1;
+				$(".exp_scale").each(function() {
+					var $this=$(this);
+					if(!$this.hasClass("b_to_t")){
+						i++;
+						setTimeout(function(){
+					   $this.addClass("b_to_t");
+					   },200*i);
+					}
+                });
+				$("#exp_list_to").fadeIn(800).delay(500).fadeTo(300,0.3);
+			}
+			if(index==5){
+				$("aside a").eq(4).addClass("selected").siblings().removeClass("selected");
+				$("#demo_content h1").after("<div class='title_en'><h2>· Demo ·</h2></div>");
+				$(".title_en").animate({width:"130px"},800,function(){
+					$(".title_en h2").slideDown(400);
+				});	
+				var i=-1;
+				$(".demo_scale").each(function() {
+					var $this=$(this);
+					if(!$this.hasClass("b_to_t")){
+						i++;
+						setTimeout(function(){
+					   $this.addClass("b_to_t");
+					   },200*i);
+					}
+				})
+			}
+			if(index==6){
+				$("aside a").eq(5).addClass("selected").siblings().removeClass("selected");
+				$("#contact_content h1").after("<div class='title_en'><h2>· Contact me ·</h2></div>");
+				$(".title_en").animate({width:"130px"},800,function(){
+					$(".title_en h2").slideDown(400);
+				});	
+				var i=-1;
+				$("#contact_head1").addClass("b_to_t");
+				$("#contact_head2 span").each(function(){
+					var $this=$(this);
+					if(!$this.hasClass("fade_in")){
+						i++;
+						setTimeout(function(){
+					   $this.addClass("fade_in");
+					   },200*i);
+					}
+				});
+				var j=-1;
+				setTimeout(function(){
+						$(".contact_scale").each(function(){
+							var $this=$(this);
+							if(!$this.hasClass("fade_in")){
+								j++;
+								setTimeout(function(){
+					   				$this.addClass("fade_in");
+					   			},350*j);
+							}
+						});
+				},70);
+			}
+		},
+		onLeave:function(index , nextIndex, direction){
+			if(index==2||index==3||index==4||index==5||index==6){
+				$(".title_en").remove();	
+			}
+		}
+	});
+});
+//顶部标题文字切换
+	$("#header_p").mouseover(function(){
+		$("#header_p1").html("Resume");
+		$("#header_p2").html("前端工程师");
+	}).mouseout(function(){
+		$("#header_p1").html("F2E");
+		$("#header_p2").html("个人简历");	
+	})
+//顶部导航取消
+	$("header nav a:not(':first')").click(function(){
+		alert("正在努力建设中...请稍等");
+		return false;
+	})
+//侧边导航文字切换
+	$("aside a").hover(function(){
+		$(this).find("b").fadeToggle(200,"easeInOutCubic");
+	})
+// 头像切换
+	$("#home_photo2").hover(function(){
+		$(this).fadeTo(800,1);
+		},function(){
+			$(this).stop(true,false).fadeTo(800,0);
+	});
+// 技能明细切换
+	$(".skill_icon").click(function(){
+		$(".skill_int").each(function(){
+			if($(this).is(":visible")){
+				$(this).slideUp(200);
+				$(this).prev().removeClass("skill_flag_scale");
+			}
+		});
+		if($(this).siblings(".skill_int").is(":hidden")){
+			$(this).siblings(".skill_int").slideDown(400);
+			$(this).siblings(".skill_flag").addClass("skill_flag_scale");
+		}else{
+			$(this).siblings(".skill_int").slideUp(200);
+			$(this).siblings(".skill_flag").removeClass("skill_flag_scale");
+		}
+	})
+// 图片轮播
+	$("#exp_list_slider").width($(".exp_list").width());
+	$("#exp_list_content").width($(".exp_list").width()*3);
+	$("#exp_list_slider_content").mouseenter(function(){
+		$("#exp_list_to").stop(true,false).fadeTo(700,1);
+	}).mouseleave(function(){
+		$("#exp_list_to").stop(true,false).fadeTo(700,0.1);
+	})
+	var page=1;
+	$("#exp_timeline a").click(function(){
+		$("#exp_list_content").stop(true,false).animate({left:-$(".exp_list").width()*$(this).index()},2000,"easeInOutCubic");
+		page=$(this).index()+1;
+		});
+	$("#exp_list_toleft").click(function()
+    {
+		if(!$("#exp_list_content").is(":animated")){
+			if(page==1){
+				$("#exp_list_content").animate({left:"+=50"},200,function(){
+					$(this).animate({left:"-=50"},200);
+				});
+				return false;
+			}	
+			$("#exp_list_content").animate({left:"+="+$(".exp_list").width()});
+			page--;
+		}
+	});
+	$("#exp_list_toright").click(function(){
+		if(!$("#exp_list_content").is(":animated")){
+			if(page==3){
+				$("#exp_list_content").animate({left:"-=50"},200,function(){
+					$(this).animate({left:"+=50"},200);
+				});
+				return;
+			}
+			$("#exp_list_content").animate({left:"-="+$(".exp_list").width()});
+			page++;
+		}
+	});
+// 时光轴
+	var x=10;
+	var y=20;
+	$("#exp_timeline a").mouseover(function(e){
+		this.aa=this.title;
+		this.title="";
+		$("body").append("<div class='exp_timeline_title'>"+this.aa+"</div>");	
+		$(".exp_timeline_title").css({
+			"top":e.pageY+y+"px",
+			"left":e.pageX+x+"px"
+		}).show("fast");
+	}).mouseout(function(){
+		this.title=this.aa;
+		$(".exp_timeline_title").remove();
+	}).mousemove(function(e){
+		$(".exp_timeline_title").css({
+			"top":e.pageY+y+"px",
+			"left":e.pageX+x+"px"
+		});
+	}).click(function(){
+		return false;
+	});
+// 点击留言
+	$("#contact_message1").click(function(){
+		$(this).fadeOut(200,function(){
+			$("#contact_form").fadeIn(200);
+		})
+	});
+// 提交表单
+	$("#contact_submit").click(function(){
+		$.get("ajax/get.php",function(){
+			$("#contact_form").fadeOut(200,function(){
+				$("#contact_message2").fadeIn(200);
+				});
+			});
+	});
+//内容适应居中
+	var size=$(function(){
+		$("aside").css({"top":($(".active").height()-$("aside").height())/2});
+		$("#home_content").css({"padding-top":($(".active").height()-$("#home_content").height())/6});
+		$("#about_content").css({"padding-top":($(".active").height()-$("#about_content").height())/6});
+		$("#skill_content").css({"padding-top":($(".active").height()-$("#skill_content").height())/6});
+		$("#exp_content").css({"padding-top":($(".active").height()-$("#exp_content").height())/6});
+		$("#demo_content").css({"padding-top":($(".active").height()-$("#demo_content").height())/6});;
+	});
+	$(window).resize(function(){
+		size();
+	});
