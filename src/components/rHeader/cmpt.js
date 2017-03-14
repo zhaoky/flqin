3cd3a48cf84c624bacb911c5fe8c0321a8f42204
zky <623212389@qq.com>
2017-03-15 00:28:54 +0800

    开发

--- src/components/rHeader/cmpt.js
+++ src/components/rHeader/cmpt.js
@@ -2,11 +2,9 @@ export default angular
 	.module("rHeader", [])
 	.component("resumeHeader", {
 		templateUrl : "components/rHeader/cmpt.html",
-		controllerAs: "rh",
 		controller  : ResumeHeaderCtrl
 	})
 	.name;
 
 function ResumeHeaderCtrl() {
-	console.log("Header");
 }
