9f577028e15a552228eb5d09932b8f9ee733874a
zky <623212389@qq.com>
2017-03-16 00:40:28 +0800

    开发中....

--- src/components/+rExperience/cmpt.js
+++ src/components/+rExperience/cmpt.js
@@ -9,8 +9,8 @@ export default angular
 		}
 	})
 	.directive("touchThreeD",[
-		
-		function(){
+		"$window",
+		function($window){
 			function link($scope,ele){
 				
 				let
@@ -18,7 +18,6 @@ export default angular
 				    bannerHeight,
 				    offsetLeft,
 					offsetTop;
-				console.log(ele);
 				$scope.$watch("$ctrl.pageIndex",function(newV){
 					if(newV !== 4){
 						return;
@@ -27,20 +26,31 @@ export default angular
 					bannerHeight = ele[0].offsetHeight;
 					offsetLeft = ele[0].offsetLeft;
 					offsetTop = ele[0].offsetTop;
-				
 				});
 				
-				ele[0].addEventListener("mousemove",function(evt){
-					let
-						pageX = evt.pageX,
-					    pageY = evt.pageY,
-						x = pageX - offsetLeft - bannerWidth/2,
-						y = pageY - offsetTop - bannerHeight/2;
-					
-					ele[0].style.transform = "rotateY(" + x/50 + "deg) rotateX(" + y/50 + "deg)";
-				})
-				
-			
+				ele[0].addEventListener("mousemove",mousemoveHandler);
+				ele[0].addEventListener("mouseout",mouseoutHandler);
+
+				function mousemoveHandler(evt){
+                    let
+                        pageX = evt.pageX,
+                        pageY = evt.pageY,
+                        x = pageX - offsetLeft - bannerWidth/2,
+                        y = bannerHeight/2-pageY + offsetTop+140;
+                    ele[0].style.transform = "rotateY(" + x/50 + "deg) rotateX(" + y/50 + "deg)";
+                }
+
+                function mouseoutHandler(evt){
+                    ele[0].style.transform = "rotateY(0deg) rotateX(0deg)";
+                }
+
+                $window.onresize= onResize;
+
+                function onResize(){
+                    offsetLeft = ele[0].offsetLeft;
+                    offsetTop = ele[0].offsetTop;
+				}
+
 			}
 			return{
 				restrict:"A",
