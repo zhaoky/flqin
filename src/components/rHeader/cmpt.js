aa33f926ef8270282b32983e520f4edd04f9828c
zky <623212389@qq.com>
2017-03-17 00:44:37 +0800

    开发中....

--- src/components/rHeader/cmpt.js
+++ src/components/rHeader/cmpt.js
@@ -1,10 +1,64 @@
 export default angular
-	.module("rHeader", [])
-	.component("resumeHeader", {
-		templateUrl : "components/rHeader/cmpt.html",
-		controller  : ResumeHeaderCtrl
-	})
-	.name;
+    .module("rHeader", [])
+    .component("resumeHeader", {
+        templateUrl: "components/rHeader/cmpt.html",
+        controller: ResumeHeaderCtrl,
+        bindings: {
+            pageIndex: "="
+        }
+    })
+    .directive("opacityCallback", [
+        function () {
+            function link($scope, ele) {
+                let vm = $scope.$ctrl;
+                ele[0].addEventListener("webkitTransitionEnd", transitionEndHandler);
+                function transitionEndHandler() {
+                    if (vm.isShowNav) {
+                        ele[0].style.display = vm.isShowNav ? "block" : "none";
+                    }
+                }
+            }
+            return {
+                restrict: "A",
+                link: link
+            }
+        }
+    ])
+    .directive("selectedNavItem", [
+        function () {
 
-function ResumeHeaderCtrl() {
+            function link($scope, ele) {
+                let vm = $scope.$ctrl;
+
+                ele[0].addEventListener("click", clickHandler);
+                function clickHandler(evt) {
+                    if (evt.target.nodeName != "LI") {
+                        return;
+                    }
+                    (evt.target.dataset.index !== vm.pageIndex) && (vm.pageIndex = evt.target.dataset.index);
+                    vm.isShowNav = false;
+                    $scope.$apply();
+                }
+            }
+
+            return {
+                restrict: "A",
+                link: link
+            }
+        }
+    ])
+    .name;
+
+ResumeHeaderCtrl.$inject = ["resumeData","$scope"];
+
+function ResumeHeaderCtrl(resumeData) {
+
+    let vm = this;
+
+    console.log(vm);
+
+    resumeData.extend(vm, resumeData.data.cn.header);
+
+    vm.pageIndex = 0;
+
 }
--- src/components/zkyResume/cmpt.js
+++ src/components/zkyResume/cmpt.js
@@ -8,18 +8,45 @@ export default angular
 		rFooter,
 		rMain
 	])
-	.service(resumeData,[
+	.service("resumeData",[
 		function(){
-			var data = {
+            this.data = {
 				cn:{
 					header:{
 						langList:["中","英"],
 						titleList:["首页","我的信息","我的技能","我的经历","我的作品","联系我"]
 					},
 					footer:{},
-					default:{},
-					info:{},
-					skill:{},
+					default:{
+                    	quote:"生活是一种绵延不绝的渴望，渴望不断上升，变得更伟大而高贵。",
+						desList:[
+							"我叫赵柯宇",
+							"一名前端研发工程师",
+							"zhaoky2016@163.com"
+						]
+					},
+					info:{
+						infoList:[
+                            {icon:"1.png",key:"年龄",value:"26岁"},
+							{icon:"2.png",key:"学历",value:"本科"},
+							{icon:"3.png",key:"坐标",value:"成都"},
+							{icon:"4.png",key:"状态",value:"在职"}
+						],
+                        desList:[
+                            "三年互联网经验,两年半全职前端开发经验",
+                            "项目常用原生JS开发,深谙组件化,模块化开发之道",
+                            "高效的自学能力,独立分析解决问题能力,代码强迫症患者"
+                        ]
+					},
+					skill:{
+                        outCircleList:["git","webpack","gulp","less","cordova","angular","nodejs","react"],
+						innerCircleList:["es6","vue","ng2","fp"],
+						desList:[
+                            "三年互联网经验，两年半全职前端开发经验",
+                            "项目常用原生JS开发，深谙组件化，模块化开发之道",
+                            "高效的自学能力,独立分析解决问题能力，代码强迫症患者"
+                        ]
+					},
 					demo:{},
 					experience:{},
 					contact:{}
@@ -27,8 +54,15 @@ export default angular
 				en:{
 					
 				}
-				
 			};
+            this.extend = function(destination,source){
+                for (let prop in source) {
+                    if (!source.hasOwnProperty(prop)) {
+                        continue;
+                    }
+                    destination[prop] = source[prop];
+                }
+			}
 		}
 	])
 	
