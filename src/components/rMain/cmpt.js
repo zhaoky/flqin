229217a7f5bcb21cd22a27dca398ca2ca740dc89
唐道远 <tangdy0351@cairenhui.com>
2017-05-15 23:48:28 +0800

    增加节流函数生成器，优化多次触发mousewheel事件造成的bug

--- src/components/rMain/cmpt.js
+++ src/components/rMain/cmpt.js
@@ -94,7 +94,7 @@ export default angular
                     nodeList[0].classList.add("cur-page");
 
                     nodeList[pageIndex + 1].classList.add("next-page");
-    
+
                     initArrow.init();
 
                     stopIosDropDown.stop(actionEvent);
@@ -102,9 +102,20 @@ export default angular
                     _consoleLog();
 
                     ele[0].addEventListener(actionEvent.event.start, touchStartHandler);
-                    ele[0].addEventListener("mousewheel", startWheelHandler);
+                    ele[0].addEventListener("mousewheel", _throttleGenerator(startWheelHandler, 100));
                 }
-                //滚轮事件
+
+                function _throttleGenerator(fn, time) {
+                  let date = new Date();
+                  return function() {
+                    let nowDate = new Date();
+                    if (nowDate - date > time) {
+                      fn(...arguments);
+                    }
+                    date = nowDate;
+                  }
+                }
+
                 function startWheelHandler(e) {
 
                     if (moving || (e.wheelDelta < 0 && pageIndex == nodeListLen - 1) || (e.wheelDelta > 0 && pageIndex == 0)) {
@@ -250,5 +261,5 @@ export default angular
             }
         }
     ])
-    
+
     .name;
